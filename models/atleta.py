b60a55c3aab855a30ef5a806a1c3822bb74353d9
Nilson Gomez <nilsongm2100@gmail.com>
2016-10-19 16:39:37 -0400

    Modificacion de atributos

--- models/atleta.py
+++ models/atleta.py
@@ -7,9 +7,10 @@ class atleta (osv.osv):
 	
 	_columns={
 		'cedula': fields.integer('Cédula de Identidad',size=10, required=True, help='Introduzca su número de de cédula de identidad'),
-		'nombre': fields.char('Nombre',size=30, required=True, help='Introduzca su nombre'),
-		'apellido':fields.char('Apellido',size=30, required=True, help='Introduzca su apellido'),
-		'fecha_nacimiento':fields.date('Fecha de nacimiento'),
+		'nombre': fields.char('Nombre',size=50, required=True, help='Introduzca su nombre completo'),
+		'disciplina': fields.char('Disciplina',size=30, required=True, help='Indique el nombre de la disciplina'),
+		'categoria': fields.char('Categoria',size=30, required=True, help='Introduzca la categoría a la que pertenece'),
+		'rendimiento':fields.char('Rendimiento',size=30, required=True, help='Introduzca su apellido'),	
 	}
 	
 	_defaults={
